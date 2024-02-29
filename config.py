3e00628ec5ad88b95ad35fe6719f941918310844
j <j>
2024-02-29 18:40:40 +0100

    extract data

--- config.py
+++ config.py
@@ -1,5 +1,2 @@
 URL = "https://ecommerce-data-generator.onrender.com"
-PARAMS = {"year": "2023", "month": 2,"day":1, "hour":1}
 COUNTRIES = ["fr", "uk", "de", "it", "es"]
-
-
--- etl/extract.py
+++ etl/extract.py
@@ -2,15 +2,12 @@ from config import URL
 
 import requests
 def get_data(country, date, hour):
+    params = {'country': country, 'year': date.split('-')[0], 'month': date.split('-')[1], 'day': date.split('-')[2], 'hour': hour }
     try:
-        response = requests.get(URL, params={'country': country,
-                                             'year': date.split('-')[0],
-                                             'month': date.split('-')[1],
-                                             'day': date.split('-')[2],
-                                             'hour': hour
-                                             })
+        response = requests.get(URL, params=params)
         response.raise_for_status()
         data = response.json()
+        data.update(params)
         print(data)
         return data
     except requests.exceptions.RequestException as e:
@@ -19,4 +16,4 @@ def get_data(country, date, hour):
 
 
 if __name__ == "__main__":
-    get_data('fr', '2023-01-01', '1')
+    get_data('fr', '2023-01-01', '01')
